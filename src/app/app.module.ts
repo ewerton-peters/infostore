1bbd4ad81fcc22e207ca9aa06852fe57185a27db
Ewerton <ewertonp282@gmail.com>
2022-08-06 23:01:43 -0300

    build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BarraPesquisaComponent } from './barra-pesquisa/barra-pesquisa.component'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NaoEncontradoComponent,
+    BarraPesquisaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/carrinho/carrinho.component.ts
@@ -0,0 +1,42 @@
+import { CurrencyPipe } from '@angular/common';
+import { identifierName } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CarrinhoService } from '../carrinho.service';
+import { IProdutoCarrinho } from '../produtos';
+
+@Component({
+  selector: 'app-carrinho',
+  templateUrl: './carrinho.component.html',
+  styleUrls: ['./carrinho.component.css']
+})
+export class CarrinhoComponent implements OnInit {
+  itensCarrinho: IProdutoCarrinho [] = [];
+  total = 0;
+
+  constructor(
+    public carrinhoService: CarrinhoService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.itensCarrinho = this.carrinhoService.obtemCarrinho();
+    this.calculaTotal();
+  }
+
+  calculaTotal() {
+    this.total = this.itensCarrinho.reduce((prev, curr) => prev + (curr.preco * curr.quantidade), 0);
+  }
+
+  removeItemCart(produtoId: number) {
+    this.itensCarrinho = this.itensCarrinho.filter(item => item.id !== produtoId);
+    this.carrinhoService.removeItemCart(produtoId);
+    this.calculaTotal();
+  }
+
+  comprar() {
+    alert("Parabéns, você finalizou sua compra!");
+    this.carrinhoService.limparCarrinho();
+    this.router.navigate(["produtos"]);
+  }
+}
